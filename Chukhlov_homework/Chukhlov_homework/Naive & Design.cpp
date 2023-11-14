ae525e3963ae3136950d9875a94c0525be161399
ex-alander <138567313+ex-alander@users.noreply.github.com>
2023-11-14 20:44:32 +0400

    good comments

--- Chukhlov_homework/Chukhlov_homework/Naive & Design.cpp
+++ Chukhlov_homework/Chukhlov_homework/Naive & Design.cpp
@@ -6,9 +6,9 @@ using namespace std;
 // design stuff
 void divide()
 {
-    cout << "------------------------------\n";
+    cout << "------------------------------------\n";
 }
-string rectangle(const string x)
+string rectangle(const string x) // to make this nice little rectangle around the function
 {
     string rect_x;
     string horiz_line = "";
@@ -24,21 +24,21 @@ string rectangle(const string x)
 
 // Naive algo
 void searchNaive(string txt, string pat) {
-    bool foundcheck = false;
+    bool foundcheck = false; // switch for checking if anything even was found or not
     int n = txt.size();
     int m = pat.size();
 
-    for (int i = 0; i <= n - m; i++) {
+    for (int i = 0; i <= n - m; i++) { // we walk through the text
         int j;
-        for (j = 0; j < m; j++) {
+        for (j = 0; j < m; j++) { // if we find a match, we keep checking if it keeps matching
             if (txt[i + j] != pat[j]) {
-                break;
+                break; // matching stopped -> move forward
             }
         }
-        if (j == m) {
-            cout << "Pattern found at index " << i << endl;
-            foundcheck = true;
+        if (j == m) { // if the index matches the length of input pattern, it means we won
+            cout << "Pattern found at index " << i << endl; // print where pattern was found
+            foundcheck = true; // we don't wanna print error if we found the pattern
         }
     }
-    if (!foundcheck) cout << "Pattern not found" << endl;
+    if (!foundcheck) cout << "Pattern not found" << endl; 
 }
