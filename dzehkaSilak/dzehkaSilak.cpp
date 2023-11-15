61086a773f705775cfc3d6d1bfd594297faa7935
Barabulkp <76599014+Kakshehka@users.noreply.github.com>
2023-11-15 21:04:34 +0300

    itog
    vse gotovo

--- dzehkaSilak/dzehkaSilak.cpp
+++ dzehkaSilak/dzehkaSilak.cpp
@@ -2,23 +2,84 @@
 #include <stdlib.h>
 #include <string.h>
 #include "shift-or.h"
+#include <iostream>
 
-int main(int argc, char** argv)
+using namespace std;
+
+void preprocess(WORD** B, const char* p, unsigned int m)
 {
-    if (argc != 3) {
-        fprintf(stderr, "Too few Arguments!\nUsage: ./shift-or TEXT PATTERN\n");
-        return 1;
+    unsigned int i;
+    WORD x = (1ul << m) - 1;
+
+    //шаг 1: заполняем массив b от 0 до м с шагом 1 
+    for (i = 0; i < CHARLIMIT; i++) {
+        (*B)[i] = x;
     }
-    unsigned int n = strlen(argv[1]);
-    unsigned int m = strlen(argv[2]);
+
+    //шаг 2: отмечаем позиции в матрице совпадающие с шаблоном
+    for (i = 0; i < m; i++) {
+        (*B)[p[i]] = (1ul << i) ^ (*B)[p[i]];
+    }
+}
+
+void search(const char* t, unsigned int n, const char* p, unsigned int m)
+{
+    WORD* B;
+    B = (WORD*)malloc(CHARLIMIT * WORDSIZE);
+
+    preprocess(&B, p, m);
+
+    //а начинает последовательность
+    WORD a = (1ul << m) - 1;
+    //z заканчивает последовательность
+    WORD z = 1ul << (m - 1);
+
+    unsigned int i;
+    for (i = 0; i < n; i++)
+    {
+        a = (a << 1) | B[t[i]];
+
+        //инвертируем а и если 1 находится в конце последовательности то найдено совпадение
+        if (~a & z) {
+            printf("Pattern found at position %u\n", i - m + 1);
+        }
+        else if ((!(~a & z)) && (i == n)) {
+            printf("Pattern not found");
+        }
+    }
+
+    free(B);
+}
+
+void Silakov(string text, string pattern) {
+    const char* c_text;
+    const char* c_pattern;
+
+    c_text = text.c_str();//Преобразуем string в const char*
+    c_pattern = pattern.c_str();
+
+    unsigned int n = strlen(c_text);//Определяем длину строки
+    unsigned int m = strlen(c_pattern);//Определяем длину шаблона
     if (m > n) {
-        fprintf(stderr, "Pattern must be shorter than text!\n");
-        return 1;
+        fprintf(stderr, "Pattern must be shorter than text!\n");//Проверяем введённые данные на корректность
     }
-    if (m > BITSINWORD) {
+    else if (m > BITSINWORD) {
         fprintf(stderr, "Pattern too long!\n");
-        return 1;
     }
-    search(argv[1], n, argv[2], m);
+    else {
+        search(c_text, n, c_pattern, m);
+    }
+}
+
+int main()
+{
+    string text;
+    string pattern;
+
+    text = "Hello, my name is Nikita";
+    pattern = "my";
+
+    Silakov(text, pattern);
+
     return 0;
 }
